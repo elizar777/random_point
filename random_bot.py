acb72698a4600a12a7cc9570662940ff2a4bdfb3
elizar777 <ahmadbekovelizar390@gmail.com>
2024-03-05 22:41:55 +0600

    like text form

---
+++ random_bot.py
@@ -0,0 +1,36 @@
+import random
+from aiogram import Bot, types, Dispatcher, executor
+import geopandas as gpd
+from shapely.geometry import Point
+from config import token
+
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+
+land = world[world['continent'] != 'Antarctica']
+
+# Получаем границы суши
+land_bounds = land.geometry.bounds
+
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+
+def generate_random_coordinates():
+    while True:
+        latitude = random.uniform(land_bounds['miny'].iloc[0], land_bounds['maxy'].iloc[0])
+        longitude = random.uniform(land_bounds['minx'].iloc[0], land_bounds['maxx'].iloc[0])
+        point = Point(float(longitude), float(latitude))
+
+        
+        if land.geometry.contains(point).any():
+            return latitude, longitude
+
+
+@dp.message_handler(commands=['send_point'])
+async def send_random_point(message: types.Message):
+    latitude, longitude = generate_random_coordinates()
+    await message.reply(f"Случайная точка на суше:\nШирота: {latitude}\nДолгота: {longitude}")
+
+
+executor.start_polling(dp)
+   
